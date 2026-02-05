c57ffc27329c6facee1b5f3a0a542cd894acd0d2
VanTho <ovantho094@gmail.com>
2026-02-05 07:34:40 +0700

    fix useQuery

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,15 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,17 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const queryClient = new QueryClient();
 
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        {children}
       </body>
     </html>
   );
